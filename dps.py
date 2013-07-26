7df78062a722522e31d253b5103ba2bc0575048a
Vahid Rafiei <vahid.rafiei@gmail.com>
2013-07-26 19:58:41 +0200

    better class description

--- dps.py
+++ dps.py
@@ -25,7 +25,7 @@ import sys
 
 class DPS(object):
 	'''
-	Class `DPS` is the main main logic with the relevant behaviours. 
+	Class `DPS` is the main logic with some relevant methods. 
 	'''
 
 	def __init__(self):
